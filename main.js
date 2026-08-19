635d4dcd9318581a9705126d6677a6860db92df2
aika-kobayashi <a-kobayashi@kyowa-e-labo.com>
2026-08-19 17:34:35 +0900

    ウィジェットの箱をデスクトップに表示

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow() {
-    const win = new BrowserWindow({
+let mainWindow;
+let widgetWindow;
+
+// Todo本体のウィンドウ
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
         webPreferences: {
@@ -10,9 +15,42 @@ function createWindow() {
         }
     });
     
-    win.loadFile('index.html');
+    mainWindow.loadFile('index.html');
+
+    //　本体を閉じた時
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
 }
 
+// デスクトップウィジェット
+
+function createWidgetWindow() {
+    widgetWindow = new BrowserWindow({
+        width: 300,
+        height: 300,
+        frame: false,
+        alwaysOnTop: true,
+        skipTaskbar: true,
+        transparent: true,
+
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    
+   widgetWindow.loadFile('widget.html');
+
+    //　ウィジェットを閉じた時
+    widgetWindow.on('closed', () => {
+        widgetWindow = null;
+    });
+}
+
+// Electron起動時
 app.whenReady().then(() => {
-    createWindow();
-})
+
+    createMainWindow();
+    createWidgetWindow();
+
+});
